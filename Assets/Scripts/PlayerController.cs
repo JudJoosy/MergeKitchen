eafbabf113d25ad3bc49414940368d66b265c39f
Spongebobswitch <fjuribe-hernandez@csuchico.edu>
2025-02-10 19:04:56 -0800

    Cooking
    Finished the cooking and began the coding for the controller(hell)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//(This script will serve to allow the player to tap on the screen ) 
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
