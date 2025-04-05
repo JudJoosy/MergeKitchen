551999d4141da0b47eab0d55b32d7a6143e65b27
Spongebobswitch <129879275+Spongebobswitch@users.noreply.github.com>
2025-04-05 06:02:41 -0700

    I mean ....I did this
    If anyone has any ideas let me know

--- Assets/Scripts/Collider2.0.cs
+++ Assets/Scripts/Collider2.0.cs
@@ -5,38 +5,39 @@ using UnityEngine;
 
 public class Collider2 : MonoBehaviour
 {
-   // private Vector2 screenBounds;
-  //  private float ObjectWidth;
-   // private float ObjectHeigth;
-   private Rigidbody rb;
-    private float dragMovment;
-    public GameObject Playercontroller;
+    // private Vector2 screenBounds;
+    //  private float ObjectWidth;
+    // private float ObjectHeigth;
+    private Vector3 offset;
+    private bool isDragging = false;
+    
     // Start is called before the first frame update
     void Start()
     {
-      // screenBounds=Camera.main.ScreenToWorldPoint (new Vector3(Screen.width, Screen.height, Camera.main.transform.position.x));
-//screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.y));
-        rb=GetComponent<Rigidbody>();
-        dragMovment = Playercontroller.GetComponent<Single>();
+        isDragging = true;
+        offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+       
+    }
+    void OnMouseUp()
+    {
+        isDragging = false;
     }
 
     // Update is called once per frame
     void Update()
-{
-    // //Vector3 currentPosition = transform.position;
-    //Vector3 viewPos = transform.position;
-    //viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1-ObjectWidth,screenBounds.x+ObjectWidth);
-   // viewPos.x = Mathf.Clamp(viewPos.y, screenBounds.y, screenBounds.y * -1);
-    // transform.position = viewPos;
-}
-    private void OnTriggerEnter(UnityEngine.Collider other)
     {
-        if (other.gameObject.tag=="Collider")
-        {
-            Debug.Log("The collision has begun");
-            rb.velocity=Vector3.zero;
-            dragMovment = 0;
-        }
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
+        float screenWidth = Camera.main.orthographicSize * 2 * 2 * Camera.main.aspect;
+        float screenHeight = Camera.main.orthographicSize * 2;
+        float left = -screenWidth / 2;
+        float top = screenHeight / 2;
+        float right = screenWidth / 2;
+        float bottom = -screenHeight / 2;
+        newPosition.x = Mathf.Clamp(newPosition.x, left, right);
+        newPosition.y=Mathf.Clamp(newPosition.y, top, bottom);
+        transform.position = newPosition;
     }
-
+   
+    
+    
 }
