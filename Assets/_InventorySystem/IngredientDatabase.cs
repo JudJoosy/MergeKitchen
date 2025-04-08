a547002c8c7d7ef34b6e890f4f0da38d29f4b4d6
Spongebobswitch <129879275+Spongebobswitch@users.noreply.github.com>
2025-04-07 19:19:04 -0700

    Revert "Merge branch 'main' of https://github.com/JudJoosy/MergeKitchen"
    This reverts commit 17537a8b11ba4759824e602588f4413d247753f3, reversing
    changes made to 19cfd8007a58b08e85cf8f8b8f3dac4735e35546.

--- Assets/_InventorySystem/IngredientDatabase.cs
+++ Assets/_InventorySystem/IngredientDatabase.cs
@@ -12,7 +12,6 @@ public class IngredientDatabase : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
