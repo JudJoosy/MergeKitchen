a2b245acc6b57326aad8bef9c836dcad92f2ce54
Spongebobswitch <129879275+Spongebobswitch@users.noreply.github.com>
2025-04-29 11:42:21 -0700

    solution for the merging and cooking pausing

--- Assets/Scripts/pauseMenu.cs
+++ Assets/Scripts/pauseMenu.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class pauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject PauseMenu;
-
+    public GameObject Cooking;
+    public GameObject Mergeing;
     public void Pause()
     {
         PauseMenu.SetActive(true);
         Time.timeScale = 0;
+        Cooking.SetActive(false);
+        Mergeing.SetActive(false);
     }
 
     public void Resume()
     {
         PauseMenu.SetActive(false);
         Time.timeScale = 1;
+        Cooking.SetActive(true);
+        Mergeing.SetActive(true);   
     }
 }
