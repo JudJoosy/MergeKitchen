12421759e7b45ebbe62c6d9bdc71629d6935c929
Spongebobswitch <129879275+Spongebobswitch@users.noreply.github.com>
2025-05-02 23:39:49 -0700

    found a solution to the pause menu

--- Assets/Scripts/pauseMenu.cs
+++ Assets/Scripts/pauseMenu.cs
@@ -1,24 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class pauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject PauseMenu;
     public GameObject Cooking;
+    public GameObject Mergeback;
     public GameObject Mergeing;
     public void Pause()
     {
+        Mergeback.SetActive(true);
         PauseMenu.SetActive(true);
         Time.timeScale = 0;
         Cooking.SetActive(false);
+        
         Mergeing.SetActive(false);
-    }
 
+    }
+   
     public void Resume()
     {
+        Mergeback.SetActive(false);
         PauseMenu.SetActive(false);
         Time.timeScale = 1;
+        
         Cooking.SetActive(true);
         Mergeing.SetActive(true);   
     }
